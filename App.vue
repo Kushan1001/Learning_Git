3a5150e450810fc03dbe251d8dc610dee9387e22
Kushan Sharma <90574896+Kushan1001@users.noreply.github.com>
2024-08-11 12:55:28 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+  <RouterView />
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+</style>
